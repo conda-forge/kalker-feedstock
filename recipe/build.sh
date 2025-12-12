e7391378fa611cf9f2125f6bb4d63757e8e1ca3e
Daniel Nachun <daniel.nachun@gmail.com>
2025-12-11 19:23:20 -0800

    try to use system libraries

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,10 +4,17 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 export CARGO_PROFILE_RELEASE_STRIP=symbols
 export CARGO_PROFILE_RELEASE_LTO=fat
-echo "[dependencies.gmp-mpfr-sys]" >> cli/Cargo.toml
-echo 'version = "1.6"' >> cli/Cargo.toml
-echo 'default-features = false' >> cli/Cargo.toml
-echo 'features = ["mpfr", "use-system-libs"]' >> cli/Cargo.toml
+export CARGO_FEATURE_USE_SYSTEM_LIBS=1
+
+# Shim hack to force gmp-mpfr-sys crate to use CFLAGS and LDFLAGS
+tee ${BUILD_PREFIX}/bin/cc_shim << EOF
+#!/bin/sh
+exec ${CC} ${CFLAGS} ${LDFLAGS} "\$@"
+EOF
+chmod +x ${BUILD_PREFIX}/bin/cc_shim
+export CC=${BUILD_PREFIX}/bin/cc_shim
+
+export RUSTFLAGS="-L${PREFIX}/lib"
 
 # check licenses
 cargo-bundle-licenses \
