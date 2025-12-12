bc6a9bb80beaba2acafa0205257bc26991906167
Daniel Nachun <daniel.nachun@gmail.com>
2025-12-11 18:49:20 -0800

    try another fix

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,10 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 export CARGO_PROFILE_RELEASE_STRIP=symbols
 export CARGO_PROFILE_RELEASE_LTO=fat
-sed -i 's/\[dependencies\]/[dependencies]\ngmp-mpfr-sys = { version = "*", features = ["mpfr", "force-cross"], optional = true }/' kalk/Cargo.toml
+echo "[dependencies.gmp-mpfr-sys]" >> cli/Cargo.toml
+echo 'version = "1.6"' >> cli/Cargo.toml
+echo 'default-features = false' >> cli/Cargo.toml
+echo 'features = ["mpfr", "force-cross-"]' >> cli/Cargo.toml
 
 # check licenses
 cargo-bundle-licenses \
