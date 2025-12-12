477866ed292782b5b7990eae753cc469480c29d3
Daniel Nachun <daniel.nachun@gmail.com>
2025-12-11 18:29:48 -0800

    fix cross-compile

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 export CARGO_PROFILE_RELEASE_STRIP=symbols
 export CARGO_PROFILE_RELEASE_LTO=fat
-sed -i 's/\[dependencies\]/[dependencies]\ngmp-mpfr-sys = { version = "*", features = ["*"], optional = true }/' kalk/Cargo.toml
+sed -i 's/\[dependencies\]/[dependencies]\ngmp-mpfr-sys = { version = "*", features = ["mpfr", "force-cross"], optional = true }/' kalk/Cargo.toml
 
 # check licenses
 cargo-bundle-licenses \
